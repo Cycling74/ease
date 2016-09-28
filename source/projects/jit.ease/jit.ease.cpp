85b1e2b96d26042746b157320d1cc75a5410f44d
Timothy Place <tim@cycling74.com>
2016-09-28 10:36:55 -0700

    jit.ease: initial

--- source/projects/jit.ease/jit.ease.cpp
+++ source/projects/jit.ease/jit.ease.cpp
@@ -1,63 +1,46 @@
-/// @file	
+/// @file
 ///	@ingroup 	minexamples
 ///	@copyright	Copyright (c) 2016, Cycling '74
 /// @author		Timothy Place
 ///	@license	Usage of this file and its contents is governed by the MIT License
 
-#include "c74_min.h"
+#include "../ease_base.h"
 
-using namespace c74::min;
 
-class hello_world : public object<hello_world> {
+class ease : public ease_base<ease>, public matrix_operator {
 public:
 
-	MIN_DESCRIPTION { "Post to the Max Console." };
-	MIN_TAGS		{ "utilities" };
+	MIN_DESCRIPTION { "Generate or apply an easing function." };
+	MIN_TAGS		{ "functions, timing" };
 	MIN_AUTHOR		{ "Cycling '74" };
-	MIN_RELATED		{ "print, jit.print, dict.print" };
-	
-	inlet	input	{ this, "(bang) post greeting to the max console" };
-	outlet	output	{ this, "(anything) output the message which is posted to the max console" };
+	MIN_RELATED		{ "ease, ease~, line, line~, curve~" };
+
+	inlet	input	{ this, "(matrix) apply easing function.", "matrix" };
+	outlet	output	{ this, "(matrix) easing function", "matrix" };
 
 
-	// post to max window == but only when the class is loaded the first time
-	message maxclass_setup { this, "maxclass_setup",
-		MIN_FUNCTION {
-			cout << "hello world" << endl;
-			return {};
-		}
-	};
-
-
-	// define an optional argument for setting the message
-	argument<number> greeting_arg	{ this, "greeting", "Initial value for the greeting attribute.",
+	argument<number> function_arg	{ this, "function", "Initial easing function to use.",
 		MIN_ARGUMENT_FUNCTION {
-			greeting = arg;
+			easing_function = arg;
 		}
 	};
 
 
-	// the actual attribute for the message
-	attribute<symbol> greeting { this, "greeting", "hello world",
-		description {
-			"Greeting to be posted. "
-			"The greeting will be posted to the Max console when a bang is received."
-		},
-	};
+	sample operator()(sample x) {
+		return apply_easing_function(x);
+	}
 
 
-	// respond to the bang message to do something
-	message bang { this, "bang", "Post the greeting.",
-		MIN_FUNCTION {
-			symbol the_greeting = greeting; // fetch the symbol itself from the attribute named greeting
+	template<class matrix_type, size_t planecount>
+	cell<matrix_type,planecount> calc_cell(cell<matrix_type,planecount> input, const matrix_info& info, matrix_coord& position) {
+		cell<matrix_type,planecount> output;
 
-			cout << the_greeting << endl;	// post to the max console
-			output.send(the_greeting);		// send out our outlet
-			return {};
-		}
-	};
+		for (auto plane=0; plane<info.planecount(); ++plane)
+			output[plane] = apply_easing_function(input[plane]);
+		return output;
+	}
 
 };
 
 
-MIN_EXTERNAL(hello_world);
+MIN_EXTERNAL(ease);
