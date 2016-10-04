213715ab8c6ca43a89ae3380d2f104a2818cc50a
Timothy Place <tim@cycling74.com>
2016-10-04 09:26:11 -0500

    validity check on range attrs, added some doxygen documentation to the shared code

--- source/projects/ease_base.h
+++ source/projects/ease_base.h
@@ -13,24 +13,35 @@ class ease_base : public object<derived_min_class_type> {
 public:
 
 	argument<number> function_arg	{ this, "function", "Initial easing function to use." };
-	argument<number> inlow_arg { this, "input_range[low]", "Initial low value for the input range." };
-	argument<number> inhigh_arg { this, "input_range[high]", "Initial high value for the input range." };
-	argument<number> outlow_arg { this, "output_range[low]", "Initial low value for the output range." };
-	argument<number> outhigh_arg { this, "output_range[high]", "Initial high value for the output range." };
+	argument<number> inlow_arg		{ this, "input_range[low]", "Initial low value for the input range." };
+	argument<number> inhigh_arg		{ this, "input_range[high]", "Initial high value for the input range." };
+	argument<number> outlow_arg		{ this, "output_range[low]", "Initial low value for the output range." };
+	argument<number> outhigh_arg	{ this, "output_range[high]", "Initial high value for the output range." };
 
 	attribute<easing::function> easing_function { this, "function", easing::function::linear, easing::function_info,
 		description { "Easing function to be applied or generated." }
 	};
 
 	attribute<numbers> input_range { this, "input_range", {0.0, 1.0},
-		description { "Expected numeric range for the input." }
+		description { "Expected numeric range for the input." },
+		setter { MIN_FUNCTION {
+			return range_attr_arg_check(args);
+		}}
 	};
 
 	attribute<numbers> output_range { this, "output_range", {0.0, 1.0},
-		description { "Expected numeric range for the output." }
+		description { "Expected numeric range for the output." },
+		setter { MIN_FUNCTION {
+			return range_attr_arg_check(args);
+		}}
 	};
 
+
 protected:
+
+	/// Parse arguments a user typed in the object box to initialize the object correctly.
+	/// @param	args	The arguments
+
 	void handle_object_arguments(const atoms& args) {
 		int offset = 0;
 		int count = args.size();
@@ -67,12 +78,34 @@ protected:
 			error("incorrect argument list for object");
 	}
 
+
+	/// Utility for both range attributes to ensure valid values
+	/// @param	args	The arguments to check.
+	/// @return			The arguments with potential alterations.
+
+	atoms range_attr_arg_check(const atoms& args) {
+		if (args.size() == 1)
+			return { 0.0, args[0] };
+		else if (args.size() == 2)
+			return args;
+		else {
+			this->cerr << "incorrect number of arguments for @input_range" << endl;
+			return { 0.0, 1.0 };
+		}
+	}
+
+
+	/// Apply the easing function and additional scaling to an input value.
+	/// Both the function and the scaling ranges are defined by the attributes above.
+	/// @param	input	The input to the scaling and easing functions.
+	///	@return			The calculated output.
+
 	number apply_easing_function(number input) {
 		const numbers& input_range = this->input_range;
 		const numbers& output_range = this->output_range;
 
-		assert(input_range.size() == 2);	// TODO: enforce in the attribute<> class
-		assert(output_range.size() == 2);	// TODO: enforce in the attribute<> class
+		assert(input_range.size() == 2);
+		assert(output_range.size() == 2);
 
 		number diff = (input_range[1] - input_range[0]);
 		number scale = (diff > DBL_EPSILON || diff < -DBL_EPSILON) ? 1.0 / diff : 1.0 / DBL_EPSILON;
@@ -81,4 +114,5 @@ protected:
 
 		return (y * (output_range[1] - output_range[0])) + output_range[0];
 	}
+
 };
